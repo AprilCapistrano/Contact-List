c7794b0e6c4120f0aec5f02644b043e8e51918ac
April Capistrano <gail.capistrano13@gmail.com>
2018-03-09 22:46:22 +0800

    Integrating Firestore into the Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//export class AppComponent {
+//  title = 'app';
+//}
+
+// (Standard component import up here)
+
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 export class AppComponent {
-  title = 'app';
+  
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,35 @@
+// Other imports removed for brevity
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+  var config = {
+    apiKey: "AIzaSyAR3mHihm7lsjdXTLgAR4UgcALihbqp56s",
+    authDomain: "contact-list-88311.firebaseapp.com",
+    databaseURL: "https://contact-list-88311.firebaseio.com",
+    projectId: "contact-list-88311",
+    storageBucket: "",
+    messagingSenderId: "989033117997"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule                            // And this
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
