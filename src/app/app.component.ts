5f395388969b85d0724d9345697ce2228895d04b
April Capistrano <gail.capistrano13@gmail.com>
2018-03-10 00:09:00 +0800

    Retrieving a single document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ interface Contact {
 	mobile : number;
 	phone : number;
 }
+interface ContactId extends Contact {
+	id  : string;
+}
 
 @Component({
   selector: 'app-root',
@@ -21,7 +24,7 @@ export class AppComponent {
   title = 'app';
 
   contactsCol: AngularFirestoreCollection<Contact>;
-  contacts: Observable<Contact[]>;
+  contacts: any;
   firstName: string;
   lastName: string;
   mobile: number;
@@ -29,14 +32,30 @@ export class AppComponent {
   email: string;
   address: string; 
 
+  contactDoc: AngularFirestoreDocument<Contact>;
+  contact: Observable<Contact>;
+
   constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
   	this.contactsCol = this.afs.collection('contacts');
   	this.contacts = this.contactsCol.valueChanges();
+
+  	this.contacts = this.contactsCol.snapshotChanges().map(actions => {
+  		return actions.map(a => {
+  			const data = a.payload.doc.data() as Contact;
+  			const id = a.payload.doc.id;
+  			return { id, data };
+  		});
+  	});
   }
 
   addContact() {
     this.afs.collection('contacts').doc('my-custom-id').set({'firstName': this.firstName, 'lastName': this.lastName, 'mobile': this.mobile, 'phone': this.phone, 'email': this.email, 'address': this.address});
+  }
+
+  getContact(contactId) {
+    this.contactDoc = this.afs.doc('contacts/'+contactId);
+    this.contact = this.contactDoc.valueChanges();
   }
 }
